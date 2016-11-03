a393df597b3c8d6754527ee54ecae8edc42f89d1
GANGA C K <kochupotty@gmail.com>
2016-11-03 20:51:19 +0530

    completed

---
+++ As14.java
@@ -0,0 +1,17 @@
+package a1;
+
+public class As14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char chara= 'A';
+		for(int i=65;i<=90;i++)
+		{
+		int ascii=(int)chara;
+		System.out.println(ascii+"-"+chara);
+		chara++;
+		}
+
+	}
+
+}
